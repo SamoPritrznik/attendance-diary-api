8dd48b11ab32933a9712e75b8a7cee7348bdf7e7
Samo Pritržnik Pungaršek <50667049+SamoPritrznik@users.noreply.github.com>
2020-05-01 15:42:58 +0200

    added docs route

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+import swaggerUi from "swagger-ui-express"
+import * as SwaggerDocument from "./api/swagger.json"
+
 var express = require('express');
 var app = express();
 var db = require('./db');
 
 var Controller = require('./src/Controller');
 app.use('/diary', Controller);
+app.use('/docs',swaggerUi.serve, SwaggerDocument)
 
 module.exports = app;
