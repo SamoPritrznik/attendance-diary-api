fe7ddbea4c6a4841ad3db28ab2acedaa6d359ad2
Samo Pritržnik Pungaršek <50667049+SamoPritrznik@users.noreply.github.com>
2020-05-01 15:58:42 +0200

    deleted smthng

--- app.js
+++ app.js
@@ -2,11 +2,8 @@
 var express = require('express');
 var app = express();
 var db = require('./db');
-var swaggerUi = require('swagger-ui-express');
-var SwaggerDocument = require('./api/swagger.json');
 
 var Controller = require('./src/Controller');
 app.use('/diary', Controller);
-app.use('/docs', SwaggerDocument)
 
 module.exports = app;
