b9f88483aa49aba5fee68450d8959da4bb90a2c6
Samo Pritržnik Pungaršek <50667049+SamoPritrznik@users.noreply.github.com>
2020-05-01 15:47:31 +0200

    added a minor fix to docs

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-import swaggerUi from "swagger-ui-express"
-import * as SwaggerDocument from "./api/swagger.json"
 
 var express = require('express');
 var app = express();
 var db = require('./db');
+var swaggerUi = require('swagger-ui-express');
+var SwaggerDocument = require('./api/swagger.json');
 
 var Controller = require('./src/Controller');
 app.use('/diary', Controller);
