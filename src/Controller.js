6839ae4107acc1fcce65eb8d4e05e01c092d682c
Samo Pritržnik Pungaršek <50667049+SamoPritrznik@users.noreply.github.com>
2020-05-03 12:55:20 +0200

    trying something new

--- src/Controller.js
+++ src/Controller.js
@@ -51,8 +51,8 @@ router.delete('/admins/:id', function (req, res) {
 });
 
 //posodobi enega admina
-router.put('/admins/:id', function (req, res) {
-    Admins.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, admin) {
+router.put('/admins', function (req, res) {
+    Admins.findByIdAndUpdate(req.body.id, req.body, {new: true}, function (err, admin) {
         if (err) return res.status(500).send("There was a problem updating the user.");
         res.status(200).send(admin);
     });
