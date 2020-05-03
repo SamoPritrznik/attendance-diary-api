cf885f93619dd1f95660205a2fd8aef69ce15da7
Samo Pritržnik Pungaršek <50667049+SamoPritrznik@users.noreply.github.com>
2020-05-03 13:11:34 +0200

    fixed a few issues in controller.js

--- src/Controller.js
+++ src/Controller.js
@@ -36,8 +36,8 @@ router.get('/admins', function (req, res) {
 //vrne enega admina
 router.get('/admins/:id', function (req, res) {
     Admins.findById(req.params.id, function (err, admin) {
-        if (err) return res.status(500).send("There was a problem finding the user.");
-        if (!admin) return res.status(404).send("No user found.");
+        if (err) return res.status(500).send("Prišlo je do težav z branjem admina");
+        if (!admin) return res.status(404).send("Ni admina s takim id");
         res.status(200).send(admin);
     });
 });
@@ -45,15 +45,15 @@ router.get('/admins/:id', function (req, res) {
 //izbriše enega admina
 router.delete('/admins/:id', function (req, res) {
     Admins.findByIdAndRemove(req.params.id, function (err, admin) {
-        if (err) return res.status(500).send("There was a problem deleting the user.");
-        res.status(200).send("User "+ admin.name +" was deleted.");
+        if (err) return res.status(500).send("Prišlo je do težave z brisanjem admina");
+        res.status(200).send("ADmin "+ admin.name +" je bil zbrisan");
     });
 });
 
 //posodobi enega admina
 router.put('/admins', function (req, res) {
     Admins.findByIdAndUpdate(req.body.id, req.body, {new: true}, function (err, admin) {
-        if (err) return res.status(500).send("There was a problem updating the user.");
+        if (err) return res.status(500).send("Prišlo je do težave z posodabljanjem admina");
         res.status(200).send(admin);
     });
 });
@@ -66,7 +66,7 @@ router.post('/constructions', function (req, res) {
             used : req.body.used,
         }, 
         function (err, construction) {
-            if (err) return res.status(500).send("Prišlo je do težave z shranjevanjem admina");
+            if (err) return res.status(500).send("Prišlo je do težave z shranjevanjem gradbišča");
             res.status(200).send(construction);
         });
 });
@@ -74,7 +74,7 @@ router.post('/constructions', function (req, res) {
 //vrne vsa gradbišča
 router.get('/constructions', function (req, res) {
     Constructions.find({}, function (err, construction) {
-        if (err) return res.status(500).send("Prišlo je do težave z branjem adminov");
+        if (err) return res.status(500).send("Prišlo je do težave z branjem gradbiščev");
         res.status(200).send(construction);
     });
 });
@@ -82,8 +82,8 @@ router.get('/constructions', function (req, res) {
 //vrne eno gradbišče
 router.get('/constructions/:id', function (req, res) {
     Constructions.findById(req.params.id, function (err, construction) {
-        if (err) return res.status(500).send("There was a problem finding the user.");
-        if (!construction) return res.status(404).send("No user found.");
+        if (err) return res.status(500).send("Prišlo je do težav z branjem gradbišča");
+        if (!construction) return res.status(404).send("Ni gradbišča s takim id");
         res.status(200).send(construction);
     });
 });
@@ -91,15 +91,15 @@ router.get('/constructions/:id', function (req, res) {
 //izbriše eno gradbišče
 router.delete('/constructions/:id', function (req, res) {
     Constructions.findByIdAndRemove(req.params.id, function (err, construction) {
-        if (err) return res.status(500).send("There was a problem deleting the user.");
-        res.status(200).send("User "+ construction.site_name +" was deleted.");
+        if (err) return res.status(500).send("Prišlo je do težave z brisanjem admina");
+        res.status(200).send("Gradbišče "+ construction.site_name +" je bilo zbrisano.");
     });
 });
 
 //posodobi eno gradbišče 
-router.put('/constructions/:id', function (req, res) {
-    Constructions.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, construction) {
-        if (err) return res.status(500).send("There was a problem updating the user.");
+router.put('/constructions', function (req, res) {
+    Constructions.findByIdAndUpdate(req.body.id, req.body, {new: true}, function (err, construction) {
+        if (err) return res.status(500).send("Prišlo je do težave z posodabljanjem gradbišča");
         res.status(200).send(construction);
     });
 });
@@ -112,7 +112,7 @@ router.post('/workers', function (req, res) {
             phone_num : req.body.phone_num,
         }, 
         function (err, worker) {
-            if (err) return res.status(500).send("Prišlo je do težave z shranjevanjem admina");
+            if (err) return res.status(500).send("Prišlo je do težave z shranjevanjem delavca");
             res.status(200).send(worker);
         });
 });
@@ -120,7 +120,7 @@ router.post('/workers', function (req, res) {
 //vrne vse delavce
 router.get('/workers', function (req, res) {
     Workers.find({}, function (err, worker) {
-        if (err) return res.status(500).send("Prišlo je do težave z branjem adminov");
+        if (err) return res.status(500).send("Prišlo je do težave z branjem delavecev");
         res.status(200).send(worker);
     });
 });
@@ -128,8 +128,8 @@ router.get('/workers', function (req, res) {
 //vrne enega delavca
 router.get('/workers/:id', function (req, res) {
     Workers.findById(req.params.id, function (err, worker) {
-        if (err) return res.status(500).send("There was a problem finding the user.");
-        if (!construction) return res.status(404).send("No user found.");
+        if (err) return res.status(500).send("Prišlo je do težav z branjem delavca");
+        if (!construction) return res.status(404).send("Ni delavca s takim id.");
         res.status(200).send(worker);
     });
 });
@@ -137,27 +137,30 @@ router.get('/workers/:id', function (req, res) {
 //izbriše enega delavca
 router.delete('/workers/:id', function (req, res) {
     Workers.findByIdAndRemove(req.params.id, function (err, worker) {
-        if (err) return res.status(500).send("There was a problem deleting the user.");
-        res.status(200).send("User "+ worker.name + worker.surname +" was deleted.");
+        if (err) return res.status(500).send("Prišlo je do težave z brisanjem delavca");
+        res.status(200).send("Delavec "+ worker.name + worker.surname +" je bil zbrisan.");
     });
 });
 
 //posodobi enega delavca 
-router.put('/workers/:id', function (req, res) {
-    Workers.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, worker) {
-        if (err) return res.status(500).send("There was a problem updating the user.");
+router.put('/workers', function (req, res) {
+    Workers.findByIdAndUpdate(req.body.id, req.body, {new: true}, function (err, worker) {
+        if (err) return res.status(500).send("Prišlo je do težave z posodabljanjem delavca");
         res.status(200).send(worker);
     });
 });
 
-router.post('/workers', function (req, res) {
+//vstavi čas
+router.post('/time', function (req, res) {
     Workers.create({
-            name : req.body.name,
-            surname: req.body.surname,
-            phone_num : req.body.phone_num,
+            worker_id: req.body.worker_id,
+            admin_id: req.body.admin_id,
+            construction_id: req.body.construction_id,
+            Timestamp_date: req.body.Timestamp_date,
+            Shift: req.body.Shift
         }, 
         function (err, worker) {
-            if (err) return res.status(500).send("Prišlo je do težave z shranjevanjem admina");
+            if (err) return res.status(500).send("Prišlo je do težave z shranjevanjem časa");
             res.status(200).send(worker);
         });
 });
@@ -165,7 +168,7 @@ router.post('/workers', function (req, res) {
 //vrne vse čas
 router.get('/time', function (req, res) {
     Time.find({}, function (err, time) {
-        if (err) return res.status(500).send("Prišlo je do težave z branjem adminov");
+        if (err) return res.status(500).send("Prišlo je do težave z branjem časa");
         res.status(200).send(time);
     });
 });
@@ -173,8 +176,8 @@ router.get('/time', function (req, res) {
 //vrne en čas
 router.get('/time/:id', function (req, res) {
     Time.findById(req.params.id, function (err, time) {
-        if (err) return res.status(500).send("There was a problem finding the user.");
-        if (!construction) return res.status(404).send("No user found.");
+        if (err) return res.status(500).send("Prišlo je do težav z branjem časa");
+        if (!construction) return res.status(404).send("Ni časa s takim id");
         res.status(200).send(time);
     });
 });
@@ -182,17 +185,17 @@ router.get('/time/:id', function (req, res) {
 //izbriše en čas
 router.delete('/time/:id', function (req, res) {
     Time.findByIdAndRemove(req.params.id, function (err, time) {
-        if (err) return res.status(500).send("There was a problem deleting the user.");
-        res.status(200).send("User "+ time +" was deleted.");
+        if (err) return res.status(500).send("Prišlo je do težav z brisanjem časa");
+        res.status(200).send("Čas "+ time.Timestamp_date + " " + Workers.findById(time.worker_id) +" je bil zbrisan.");
     });
 });
 
 router.use('/docs', swaggerUi.serve, swaggerUi.setup(SwaggerDocument));
 
 //posodobi en čas
-router.put('/time/:id', function (req, res) {
-    Time.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, time) {
-        if (err) return res.status(500).send("There was a problem updating the user.");
+router.put('/time', function (req, res) {
+    Time.findByIdAndUpdate(req.body.id, req.body, {new: true}, function (err, time) {
+        if (err) return res.status(500).send("Prišlo je do težave z posodabljanjem časa");
         res.status(200).send(time);
     });
 });
